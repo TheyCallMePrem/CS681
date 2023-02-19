ea4ba078b38db75e6bbd23d7514eddf15bc1bfa1
Prem D <prem.desai0008@gmail.com>
2023-02-18 23:54:59 -0500

    Delete HW03 directory

--- HW03/src/hw03/DSummary.java
+++
@@ -1,8 +0,0 @@
-package hw03;
-
-public class DSummary extends Summary {
-    public DSummary(Double open, Double high, Double low, Double close) {
-        super(open, high, low, close);
-    }
-}
-
--- HW03/src/hw03/Observer.java
+++
@@ -1,6 +0,0 @@
-package hw03;
-
-public interface Observer<T> {
-	public void update(Observable<T> sender, T event);
-}
-
