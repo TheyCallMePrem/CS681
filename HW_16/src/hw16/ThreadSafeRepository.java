01643372640e7ab8b96315167b5f4f244177d4e2
Prem D <p.desai005@umb.edu>
2023-05-13 11:38:36 -0400

    Update ThreadSafeRepository.java

--- HW_16/src/hw16/ThreadSafeRepository.java
+++ HW_16/src/hw16/ThreadSafeRepository.java
@@ -26,4 +26,28 @@ public class ThreadSafeRepository {
             lock.unlock();
         }
     }
+    public static void main(String[] args) {
+        ThreadSafeRepository repository = new ThreadSafeRepository();
+        
+        // Spawn multiple threads to push files to the repository
+        for (int i = 0; i < 10; i++) {
+            int threadNum = i;
+            new Thread(() -> {
+                String filename = "file" + threadNum + ".txt";
+                String content = "Hello from thread " + threadNum;
+                repository.push(filename, content);
+                System.out.println("Thread " + threadNum + " pushed file: " + filename);
+            }).start();
+        }
+    
+        // Spawn multiple threads to pull files from the repository
+        for (int i = 0; i < 10; i++) {
+            int threadNum = i;
+            new Thread(() -> {
+                String filename = "file" + threadNum + ".txt";
+                String content = repository.pull(filename);
+                System.out.println("Thread " + threadNum + " pulled content: " + content);
+            }).start();
+        }
+    }
 }
